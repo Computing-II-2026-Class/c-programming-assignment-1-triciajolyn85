00e3a7cf1c190ac0d15e18db0a853f2086572f0d
triciajolyn85 <triciajolyn85@gmail.com>
2026-03-24 20:12:29 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,23 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name:BASAIJA PATRICIA */
+/* Student Number: 25/U/BIE/01362/PE*/
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+     int units;
+    float cost_per_unit,total_bill;
+    printf("Enter units consumed:");
+    scanf("%d",&units);
+
+    printf("Enter cost per unit:");
+    scanf("%f", &cost_per_unit);
+
+    total_bill =units*cost_per_unit;
+    
+
+    printf("\nTotal bill:%2fUGX\n",total_bill);
+    
 
     return 0;
 }
